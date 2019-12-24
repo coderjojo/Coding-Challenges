66bd2bb0bf58edc17a21ee501e83c9f0c6856fce
coderjojo <mail2vedj@gmail.com>
2019-12-24 19:49:47 +0530

    2Sum Added

--- leetcode/twoSum.cpp
+++ leetcode/twoSum.cpp
@@ -19,5 +19,29 @@ public:
     }
 };
 
-//Optimized O(nlogn)
+/*
+a == b
+a > b
+a < b */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       unordered_map<int, int> mp;
+        vector<int> V;
+        
+        for (int i=0; i<nums.size(); i++) {
+            if ( mp.find(target - nums[i]) == mp.end() ) 
+                //if not add to map 
+                mp[nums[i]] = i;
+            
+            else{
+                //else add index to vector 
+                V.push_back(mp[target - nums[i]]);
+                V.push_back(i);
+            }
+        }
+        return V;
+    }
+};
 
+//Optimized O(nlogn)
