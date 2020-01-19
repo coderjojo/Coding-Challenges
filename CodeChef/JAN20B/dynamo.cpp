dfcf0b42ac200621cdc6d66bf94bc3d379dfd587
coderjojo <45591748+coderjojo@users.noreply.github.com>
2020-01-19 19:06:55 +0530

    codechef sol-pull

---
+++ CodeChef/JAN20B/dynamo.cpp
@@ -0,0 +1,54 @@
+
+	#include <bits/stdc++.h>
+	#include <cstdio>
+	#include <cstring>
+	#include <cmath>
+	#include <cstring>
+	#include <chrono>
+	#include <complex>
+	#define endl "\n"
+	#define ll long long int
+	#define vi vector<int>
+	#define vll vector<ll>
+	#define vvi vector < vi >
+	#define pii pair<int,int>
+	#define pll pair<long long, long long>
+	#define mod 1000000007
+	#define inf 1000000000000000001;
+	#define all(c) c.begin(),c.end()
+	#define mp(x,y) make_pair(x,y)
+	#define mem(a,val) memset(a,val,sizeof(a))
+	#define eb emplace_back
+	#define f first
+	#define s second
+	
+	using namespace std;
+	int main()
+	{
+		std::ios::sync_with_stdio(false);
+		int T;
+		cin>>T;
+		// cin.ignore(); must be there when using getline(cin, s)
+		while(T--)
+		{
+	    ll a,b,c,d,n,sum = 1, s = 1;
+        cin>>n>>a;
+
+        for(ll i = 0; i < n; i++)
+            s*=10;
+
+        cout<<a+2*s<<endl;
+        cout.flush();
+
+        cin>>c;
+        cout<<s-c<<endl;
+        cout.flush();
+
+        cin>>d;
+        cout<<s-d<<endl;
+        cout.flush();
+        cin>>sum;
+
+		}
+		return 0;
+	}
