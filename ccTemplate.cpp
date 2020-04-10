d973f518b1760a55ddacefbcb4db1154401cbb6f
coderjojo <mail2vedj@gmail.com>
2020-04-10 19:45:57 +0530

    Update Template

--- ccTemplate.cpp
+++ ccTemplate.cpp
@@ -5,6 +5,7 @@ typedef long long ll;
 
 #define M 1000000007
 #define PI 3.1415926
+#define vi vector<int>  
 
 #define pb push_back
 #define mp make_pair
@@ -16,8 +17,10 @@ typedef long long ll;
 
 #define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
-#define forf(x,n) for(int x = 0; x < n; ++x)
-#define forb(x,n) for(int x = n-1; x >= 0; --x)
+#define forf(x,n) for(int i = 0; i < n; ++x)
+#define forb(x,n) for(int i = n-1; i >= 0; --x)
+#define forv(v,n) for(int i = v; i < n; ++i)
+
 
 #define itr(c,it) for( auto it = c.begin(); it != c.end(); ++it)
 
@@ -25,5 +28,12 @@ typedef priority_queue <int> pq;
 
 int main()
 {
-	fastio;
+        fastio;
+        int tc;
+        std::cin >> tc;
+
+        while(tc--){
+        }
+
+        return 0;
 }
